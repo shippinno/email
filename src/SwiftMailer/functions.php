b882cefdbcfd1d343eb220b2511f23506aba3828
Hirofumi Tanigami <tanigami@gmail.com>
2018-11-06 11:03:35 +0900

    functions

--- src/SwiftMailer/functions.php
+++ src/SwiftMailer/functions.php
@@ -1,16 +1,17 @@
 <?php
 declare(strict_types=1);
 
-namespace Shippinno\Email\SwiftMailer;
+namespace Shippinno\Email\SwiftMailer {
 
-use Swift;
-use Swift_DependencyContainer;
+    use Swift;
+    use Swift_DependencyContainer;
 
-function initSwiftWithNonRFCEmailValidator()
-{
-    Swift::init(function () {
-        Swift_DependencyContainer::getInstance()
-            ->register('email.validator')
-            ->asSharedInstanceOf(NonRFCEmailValidator::class);
-    });
+    function init_swift_with_non_rfc_email_validator()
+    {
+        Swift::init(function () {
+            Swift_DependencyContainer::getInstance()
+                ->register('email.validator')
+                ->asSharedInstanceOf(NonRFCEmailValidator::class);
+        });
+    }
 }
---
+++ src/SwiftMailer/functions_include.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+if (!function_exists('Shippinno\Email\SwiftMailer\init_swift_with_non_rfc_email_validator')) {
+    require __DIR__ . '/functions.php';
+}
