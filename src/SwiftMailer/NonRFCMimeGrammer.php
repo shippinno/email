31e7ffaa3bce3f104f7c2ead776bb7ae4a8f19d4
Hirofumi Tanigami <tanigami@gmail.com>
2018-12-10 11:14:02 +0900

    super loose non RFC mime grammer

--- src/SwiftMailer/NonRFCMimeGrammer.php
+++ src/SwiftMailer/NonRFCMimeGrammer.php
@@ -5,9 +5,6 @@ namespace Shippinno\Email\SwiftMailer;
 
 use Swift_Mime_Grammar;
 
-/**
- * @see https://gist.github.com/basuke/16ad5c07fba3f029e729
- */
 class NonRFCMimeGrammer extends Swift_Mime_Grammar
 {
     /**
@@ -17,22 +14,7 @@ class NonRFCMimeGrammer extends Swift_Mime_Grammar
     {
         switch ($name) {
             case 'addr-spec':
-                /** @noinspection PhpUnhandledExceptionInspection */
-                $spec = parent::getDefinition($name);
-                $fuzzy_domains = array(
-                    'docomo\.ne\.jp',
-                    'ezweb\.ne\.jp',
-                );
-                $atext = $this->getDefinition('atext');
-                $CFWS = $this->getDefinition('CFWS');
-                $invalid_dot_atom_text = '(?:\.*'. $atext. '+'. '(\.+'. $atext. '+)*\.*)';
-                $invalid_dot_atom = '(?:'. $CFWS. '?'. $invalid_dot_atom_text. '+'. $CFWS. '?)';
-                $quoted_string = $this->getDefinition('quoted-string');
-                $local_part = '(?:'. $invalid_dot_atom. '|'. $quoted_string. ')';
-                $domain_part = '(?:'. implode('|', $fuzzy_domains). ')';
-                $spec = '(?:'. $spec. '|'. $local_part. '@'. $domain_part. ')';
-
-                return $spec;
+                return '.+\@\S+\.\S+';
             default:
                 /** @noinspection PhpUnhandledExceptionInspection */
                 return parent::getDefinition($name);
